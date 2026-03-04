5f409cf6bd0704dbb443036ec4672381976a00e2
𝖒𝖎𝖍𝖓𝖊𝖆 𝖒𝖔𝖘𝖔 <sons.mosorescu@gmail.com>
2026-03-04 13:21:40 +0200

    finished tokenize function

--- src/jsoc.c
+++ src/jsoc.c
@@ -9,15 +9,16 @@
 #define MAX_READ 1024 * 50
 
 typedef enum {
-    OPEN_CURLY,
-    CLOSED_CURLY,
-    // OPEN_PAREN,
-    // CLOSED_PAREN,
-    OPEN_BRACKET, // or ARRAY_START
-    CLOSED_BRACKET,
-    COMMA,
-    NUMBER,
-    STRING,
+    JSON_OCURLY,
+    JSON_CCURLY,
+    JSON_OBRACKET,
+    JSON_CBRACKET,
+    JSON_COMMA,
+    JSON_NUMBER,
+    JSON_STRING,
+    JSON_TRUE,
+    JSON_FALSE,
+    JSON_NULL,
     UNKNOWN
 } TokenType;
 
@@ -40,6 +41,22 @@ typedef enum {
     STATE_IN_UNKNOWN
 } TokenizerState;
 
+// typedef struct {
+//     int pos, col, row;
+// } Cur;
+
+// typedef struct {
+//     const char *content;
+//     size_t count;
+//     const char *file_path;
+
+//     Cur cur;
+
+//     TokenType token;
+//     String_Builder string;
+//     size_t row, col;
+// } Lexer;
+
 bool is_hex(char c) {
     c = tolower(c);
     return (c == 'a') || (c == 'b') || (c == 'c') || (c == 'd') || (c == 'e') || (c == 'f') || ((0 <= c - '0') && (c - '0' <= 9));
@@ -58,12 +75,18 @@ int tokenizeJSON(char *buffer, Token* tokens, int max_tokens)
     int token_count = 0;
     char temp[256];
     int temp_idx;
+    int line_count = 0, col_pos = 0;
 
     // int token
     while (*ptr != '\0' && token_count < max_tokens)
     {
         bool is_minus = false;
         bool is_period = false;
+        if (*ptr == '\n') {
+            line_count++;
+            col_pos = 0;
+        }
+        
         switch (state) {
             case STATE_START: {
                 if (isspace(*ptr)) ptr++; // handled WHITESPACE
@@ -81,20 +104,32 @@ int tokenizeJSON(char *buffer, Token* tokens, int max_tokens)
                     ptr++; // Skip opening quote
                 } else if (*ptr == '[') {
                     state = STATE_IN_ARRAY;
-                    tokens[token_count].type = OPEN_BRACKET; // FUCK
+                    tokens[token_count++].type = JSON_OBRACKET; // FUCK
                     ptr++; // Skip opening bracket
                     // temp_idx = 0;
                 } else if (*ptr == ',') {
                     // assert that we are inside an array
                     // or an object
                     // probably will be done by the parser tho
-                    tokens[token_count].type = COMMA;
+                    tokens[token_count++].type = JSON_COMMA;
                     ptr++; // Skip comma
                 } else if (*ptr == '{') {
                     state = STATE_IN_OBJECT;
-                    tokens[token_count].type = OPEN_CURLY;
+                    tokens[token_count++].type = JSON_OCURLY;
                     ptr++; // Skip opening curly
                     // temp_idx = 0;
+                } else if (*ptr == 't') { // true
+                    state = STATE_IN_TRUE;
+                    tokens[token_count].type = JSON_TRUE;
+                } else if (*ptr == 'f') { // false
+                    state = STATE_IN_FALSE;
+                    tokens[token_count].type = JSON_FALSE;
+                } else if (*ptr == 'n') { // null
+                    state = STATE_IN_NULL;
+                    tokens[token_count].type = JSON_NULL;
+                } else {
+                    state = STATE_IN_UNKNOWN;
+                    tokens[token_count].type = UNKNOWN;
                 }
                 break;
             }
@@ -131,7 +166,7 @@ int tokenizeJSON(char *buffer, Token* tokens, int max_tokens)
                     temp[temp_idx++] = *ptr++;
                 } else {
                     temp[temp_idx] = '\0';
-                    tokens[token_count].type = NUMBER;
+                    tokens[token_count].type = JSON_NUMBER;
                     strcpy(tokens[token_count].value, temp);
                     token_count++;
                     state = STATE_START;
@@ -172,7 +207,7 @@ int tokenizeJSON(char *buffer, Token* tokens, int max_tokens)
                 }
                 if (*ptr == '"' && *(ptr - 1) != 0x5c) { // 0x5c == '\'
                     temp[temp_idx] = '\0';
-                    tokens[token_count].type = STRING;
+                    tokens[token_count].type = JSON_STRING;
                     strcpy(tokens[token_count].value, temp);
                     token_count++;
                     ptr++; // Skip closing quote
@@ -184,11 +219,11 @@ int tokenizeJSON(char *buffer, Token* tokens, int max_tokens)
             }
 
             case STATE_IN_ARRAY: {
-                if (*ptr) {}
+                
                 if (*ptr == ']') {
                     state = STATE_START;
                     ptr++; // Skip closing bracket
-                    tokens[token_count].type = CLOSED_BRACKET;
+                    tokens[token_count].type = JSON_CBRACKET;
                     token_count++;
                 } else {
                     state = STATE_START;
@@ -196,23 +231,38 @@ int tokenizeJSON(char *buffer, Token* tokens, int max_tokens)
                 break;
             }
             case STATE_IN_OBJECT: {
-
+                if (*ptr == '}') {
+                    state = STATE_START;
+                    ptr++;
+                    tokens[token_count].type = JSON_CCURLY;
+                    token_count++;
+                } else {
+                    state = STATE_START;
+                }
                 break;
             }
             case STATE_IN_TRUE: {
-
+                ptr += 4; // 4 bytes of 'true'
+                token_count++;
+                state = STATE_START;
                 break;
             }
             case STATE_IN_FALSE: {
-
+                ptr += 5; // 5 bytes of 'false'
+                token_count++;
+                state = STATE_START;
                 break;
             }
             case STATE_IN_NULL: {
-
+                ptr += 4; // 4 bytes of 'null'
+                token_count++;
+                state = STATE_START;
                 break;
             }
             case STATE_IN_UNKNOWN: {
-
+                // We dont know what the token is, we just increment
+                ptr++;
+                state = STATE_START;
                 break;
             }
         }
